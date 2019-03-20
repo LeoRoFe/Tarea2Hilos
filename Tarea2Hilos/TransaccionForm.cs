9593a0e858ecc6c9de68970b8a1e91e6092f2253
Melissa Arroliga <meliarroliga@gmail.com>
2019-03-20 14:45:12 -0600

    gui

--- Tarea2Hilos/TransaccionForm.cs
+++ Tarea2Hilos/TransaccionForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Tarea2Hilos
 {
-    public partial class Form1 : Form
+    public partial class TransaccionForm : Form
     {
-        public Form1()
+        public TransaccionForm()
         {
             InitializeComponent();
         }
