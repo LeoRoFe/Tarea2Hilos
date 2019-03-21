f7a11e81449a0c3dece777279f8aa5b3466a9677
Melissa Arroliga <meliarroliga@gmail.com>
2019-03-21 14:10:40 -0600

    .

--- Tarea2Hilos/Clases/Archivos.cs
+++ Tarea2Hilos/Clases/Archivos.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Tarea2Hilos.Clases
 {
     class Archivos
     {
-
+        //
     }
 }
